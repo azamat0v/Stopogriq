2d89da8709b86d445f1bf5399224880097a7171f
Abdulloh <92520846+azamat0v@users.noreply.github.com>
2024-02-22 18:09:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import telebot
 from telebot import types
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz
-
 TOKEN = '6854903171:AAGoQ5C2FM-YQpuaFZGZxxeGDMlAXgzhIg8'
 bot = telebot.TeleBot(TOKEN)
-
 GSPREAD_JSON = 'credentials.json'
 SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 CREDENTIALS = ServiceAccountCredentials.from_json_keyfile_name(GSPREAD_JSON, SCOPE)
 GSHEET_NAME = 'Biolarisbotusers'
-
 user_data = {}
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_id = message.from_user.id
@@ -23,91 +19,64 @@ def handle_start(message):
     if user_id not in user_data:
         user_data[user_id] = {'start_count': 0}
         user_data[user_id]['start_count'] += 1
-        username = message.from_user.username
-        user_data[user_id]['username'] = username if username else "Username yo'q"
+    username = message.from_user.username
+    user_data[user_id]['username'] = username if username else "Username yo'q"
 
-        bot.send_message(user_id, "Assalomu alaykum! Iltimos ismingizni kiriting:")
-        bot.register_next_step_handler(message, handle_name)
-
-def handle_name(message):
+    bot.send_message(user_id, "Assalomu alaykum! Iltimos ismingizni kiriting:")
+    bot.register_next_step_handler(message, handle_name)
+    def handle_name(message):
     user_id = message.from_user.id
     user_data[user_id]['name'] = message.text
-
     markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     item = types.KeyboardButton("Kontaktni ulashish", request_contact=True)
     markup.add(item)
-
     bot.send_message(user_id, "Kontaktingizni yuboring:", reply_markup=markup)
     bot.register_next_step_handler(message, handle_contact)
-
 def handle_contact(message):
     user_id = message.from_user.id
-    user_data[user_id]['name'] = message.contact.first_name  # Use contact name
-
+    if message.contact:
+        user_data[user_id]['phone'] = message.contact.phone_number
+        bot.send_message(user_id, "Faylni olish uchun pastdagi tugmani bosing👇:",
+                         reply_markup=create_file_button())
+    else:
+        bot.send_message(user_id, "Iltimos kontaktni ulashish tugmasini bosing.")
+        return
+def create_file_button():
     markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     item = types.KeyboardButton("📥 Yuklab olish", request_location=False)
     markup.add(item)
-
-    bot.send_message(user_id, "Faylni olish uchun pastdagi tugmani bosing👇:", reply_markup=markup)
-    bot.register_next_step_handler(message, handle_file)
-
+    return markup
 @bot.message_handler(func=lambda message: message.text == "📥 Yuklab olish")
 def handle_file(message):
     user_id = message.from_user.id
-
-    if 'file_sent' not in user_data[user_id] or user_data[user_id]['file_sent'] == 0:
-        # The file hasn't been sent yet
-        user_data[user_id]['file_sent'] = 1
-
-        with open('file.pdf', 'rb') as f:
-            bot.send_document(user_id, f)
-
-        remove_file_button(user_id)
-        bot.send_message(user_id, "Qiziqish bildirganingiz uchun rahmat🙂")
-
-        # Record the time the file was sent
-        user_data[user_id]['file_sent_time'] = datetime.now()
-
-        # Send all information to Google Sheets
-        send_all_information(user_id)
-    else:
-        elapsed_time = datetime.now() - user_data[user_id].get('file_sent_time', datetime.now())
-        if elapsed_time > timedelta(minutes=1):
-            user_data[user_id]['file_sent'] = 0
-            send_all_information(user_id)
-
-        # bot.send_message(user_id, "You have already sent the file. If you want to send it again, click the button.")
-
-
-
+    user_data[user_id]['file_sent'] = 1
+    send_all_information(user_id)
+    remove_file_button(user_id)
+    with open('file.pdf', 'rb') as f:
+        bot.send_document(user_id, f)
+    bot.send_message(user_id, "Qiziqish bildirganingiz uchun rahmat🙂")
 def remove_file_button(user_id):
     markup = types.ReplyKeyboardRemove(selective=False)
     bot.send_message(user_id, "Fayl yuborildi!", reply_markup=markup)
-
 def add_to_spreadsheet(user_id):
     gc = gspread.authorize(CREDENTIALS)
     spreadsheet = gc.open(GSHEET_NAME)
     data_sheet = spreadsheet.sheet1
-
     user_rows = data_sheet.col_values(2)
-
     if str(user_id) in user_rows:
         user_index = user_rows.index(str(user_id)) + 1
         existing_row = data_sheet.row_values(user_index)
-
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         name = user_data[user_id].get('name', '')
         username = user_data[user_id].get('username', '')
         phone = user_data[user_id].get('phone', '')
         file_sent = str(user_data[user_id].get('file_sent', 0))
-
         existing_row[0] = str(user_id)
         existing_row[1] = timestamp
         existing_row[2] = name
         existing_row[3] = username
         existing_row[4] = phone
         existing_row[5] = file_sent
-
         data_sheet.update(str(user_index), existing_row)
         data_sheet.format('B:B', {'numberFormat': {'type': 'DATE_TIME'}})
     else:
@@ -117,13 +86,10 @@ def add_to_spreadsheet(user_id):
         username = user_data[user_id].get('username', '')
         phone = user_data[user_id].get('phone', '')
         file_sent = str(user_data[user_id].get('file_sent', 0))
-
         new_row = [str(user_id), timestamp, name, username, phone, file_sent]
         data_sheet.append_row(new_row)
         data_sheet.format('B:B', {'numberFormat': {'type': 'DATE_TIME'}})
-
 def send_all_information(user_id):
     add_to_spreadsheet(user_id)
     del user_data[user_id]
-
 bot.polling(none_stop=True)
